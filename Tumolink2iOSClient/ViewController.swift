f6fe8b0a6982941c1e2f0aa9e182dfaad6f15c2e
Terashima <yamanogomora@gmail.com>
2019-11-29 15:32:48 +0900

    Initial Commit

---
+++ Tumolink2iOSClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tumolink2iOSClient
+//
+//  Created by 寺島 洋平 on 2019/11/29.
+//  Copyright © 2019 YoheiTerashima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

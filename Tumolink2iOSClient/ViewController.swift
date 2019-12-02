318494fef1aab927a5e9ba17de5b8c764bf7db46
Terashima <yamanogomora@gmail.com>
2019-12-02 15:21:01 +0900

    #2 先にログインフローを作成するために、初期表示のストーリーボードを変更

--- Tumolink2iOSClient/ViewController.swift
+++ Tumolink2iOSClient/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let storyboard = UIStoryboard(name: "LoginStoryboard", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "LoginVC")
+        present(controller, animated: true, completion: nil)
+    }
 
 
 }

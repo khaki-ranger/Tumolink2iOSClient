ade6ef57e58cf874bf14c21affa936ad029ab475
Terashima <yamanogomora@gmail.com>
2019-12-03 22:52:01 +0900

    #2 UIのデザインを制御するためのファイルをView層に作成

---
+++ Tumolink2iOSClient/View/RoundedViews.swift
@@ -0,0 +1,34 @@
+//
+//  RoundedViews.swift
+//  Tumolink2iOSClient
+//
+//  Created by 寺島 洋平 on 2019/12/03.
+//  Copyright © 2019 YoheiTerashima. All rights reserved.
+//
+
+import UIKit
+
+class RoundedButton : UIButton {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = 5
+    }
+}
+
+class RoundedShadowView : UIView {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = 5
+        layer.shadowColor = #colorLiteral(red: 0.6, green: 0.6, blue: 0.6, alpha: 1)
+        layer.shadowOpacity = 0.4
+        layer.shadowOffset = CGSize.zero
+        layer.shadowRadius = 3
+    }
+}
+
+class RoundedImageView: UIImageView {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = 5
+    }
+}

98e46535d17177fe0ef1bd18dde560be607eb983
Terashima <yamanogomora@gmail.com>
2019-12-12 11:14:15 +0900

    #6 タブバーコントローラーの設定

---
+++ Tumolink2iOSClient/Controller/CustomTC.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTC.swift
+//  Tumolink2iOSClient
+//
+//  Created by 寺島 洋平 on 2019/12/12.
+//  Copyright © 2019 YoheiTerashima. All rights reserved.
+//
+
+import UIKit
+
+class CustomTC: UITabBarController {
+    
+    // MART: Outlets
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: Functions
+    
+    // MARK: Actions
+
+}
